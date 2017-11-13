17d0f68d72c5f7a895017644cf6dbece785f5c92
areynoua <areynoua@ulb.ac.be>
2017-11-13 17:32:24 +0100

    magic latex table

--- doc/Report/makepdf.sh
+++ doc/Report/makepdf.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-xelatex report.tex
-xelatex report.tex
+#xelatex report.tex
+#xelatex report.tex
+#rm -f *.aux *.log *.toc
+
+xelatex report2.tex
+xelatex report2.tex
 rm -f *.aux *.log *.toc
