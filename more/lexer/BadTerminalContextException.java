9b74675dfac02cc7c688ae8ceacebba01472eda0
areynoua <areynoua@ulb.ac.be>
2017-11-25 10:06:20 +0100

    indent

--- more/lexer/BadTerminalContextException.java
+++ more/lexer/BadTerminalContextException.java
@@ -1,7 +1,7 @@
 // Example of use: if end of comment occurs outside of any comment
 public class BadTerminalContextException extends Exception {
-	public BadTerminalContextException() { super(); }
-	public BadTerminalContextException(String message) { super(message); }
-	public BadTerminalContextException(String message, Throwable cause) { super(message, cause); }
-	public BadTerminalContextException(Throwable cause) { super(cause); }
+    public BadTerminalContextException() { super(); }
+    public BadTerminalContextException(String message) { super(message); }
+    public BadTerminalContextException(String message, Throwable cause) { super(message, cause); }
+    public BadTerminalContextException(Throwable cause) { super(cause); }
 }
--- more/lexer/BadTerminalException.java
+++ more/lexer/BadTerminalException.java
@@ -1,7 +1,7 @@
 // Example of use: Non ASCII character in identifier
 public class BadTerminalException extends Exception {
-	public BadTerminalException() { super(); }
-	public BadTerminalException(String message) { super(message); }
-	public BadTerminalException(String message, Throwable cause) { super(message, cause); }
-	public BadTerminalException(Throwable cause) { super(cause); }
+    public BadTerminalException() { super(); }
+    public BadTerminalException(String message) { super(message); }
+    public BadTerminalException(String message, Throwable cause) { super(message, cause); }
+    public BadTerminalException(Throwable cause) { super(cause); }
 }
