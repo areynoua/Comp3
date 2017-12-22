c4543763a64ddab21e60d52f2ea086f173cb6897
areynoua <areynoua@ulb.ac.be>
2017-12-22 15:59:09 +0100

    tests

---
+++ more/tests.sh
@@ -0,0 +1,17 @@
+for file in ../test/*.imp;
+do
+	f=${file##*/};
+	echo test $f
+	if ! make ${f%%.imp};
+	then
+		diff /tmp/out.out ${file%%.imp}.out
+		echo
+		echo compare with
+		echo vimdiff /tmp/out.out ${file%%.imp}.out
+		echo vim -o $file ${file%%.imp}.in ${file%%.imp}.out
+		break;
+	fi;
+	echo
+	echo --------------------
+	echo
+done
